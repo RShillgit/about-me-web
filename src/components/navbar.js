37fdd4756ce317ee52e073ac3c69c1cf488c0e20
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-01-11 13:26:47 -0500

    Dropdown menu for devices below 400px width

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,112 @@
 import '../styles/navbar.css';
+import { useEffect, useState } from 'react';
 import homeBtn from '../images/home.png';
 import moon from '../images/moon.png';
 import sun from '../images/sun.png';
+import menu from '../images/menu.png';
 
 const NavBar = () => {
+
+    const [navbarLinks, setNavbarLinks] = useState('');
+    const [navbarSmall, setNavbarSmall] = useState(false);
+
+    // On mount
+    useEffect(() => {
+
+        // If initial screen width is over 400px, create normal navbar
+        if(window.innerWidth >= 400) {
+            setNavbarLinks(
+                <div className="navbar-links">
+                    <div className="navbar-individual-link">
+                        <button className="navbar-clickable" onClick={buttonClickHandler}>About</button>
+                    </div>
+                    <div className="navbar-individual-link">
+                        <button className="navbar-clickable" onClick={buttonClickHandler}>Projects</button>
+                    </div>
+                    <div className="navbar-individual-link">
+                        <button className="navbar-clickable" onClick={buttonClickHandler}>Contact</button>
+                    </div>
+                    <div className="navbar-individual-link">
+                        <button className="navbar-clickable" id="navbar-resume" onClick={buttonClickHandler}>Resume</button>
+                    </div>
+                </div>
+            )
+        }
+        // If its under 400px, set navbarSmall state to true to render options menu
+        else if (window.innerWidth < 400) setNavbarSmall(true);
+
+        // Monitor window resize
+        window.onresize = () => navbarMenuMonitor();
+
+    }, []);
+
+    // On navbarSmall state change
+    useEffect(() => {
+
+        const navbar = document.querySelector('.navbar');
+
+        // If navbarSmall is false, set links to normal format
+        if(navbarSmall === false) {
+            setNavbarLinks(
+                <div className="navbar-links">
+                    <div className="navbar-individual-link">
+                        <button className="navbar-clickable" onClick={buttonClickHandler}>About</button>
+                    </div>
+                    <div className="navbar-individual-link">
+                        <button className="navbar-clickable" onClick={buttonClickHandler}>Projects</button>
+                    </div>
+                    <div className="navbar-individual-link">
+                        <button className="navbar-clickable" onClick={buttonClickHandler}>Contact</button>
+                    </div>
+                    <div className="navbar-individual-link">
+                        <button className="navbar-clickable" id="navbar-resume" onClick={buttonClickHandler}>Resume</button>
+                    </div>
+                </div>
+            );
+
+            // Remove condensed class to navbar
+            navbar.classList.toggle('condensed');
+        }
+
+        // else if navbarSmall is true, set links to condensed format
+        else if (navbarSmall === true) {
+            setNavbarLinks(
+                <div className="navbar-links condensed">
+                    <button className="navbar-clickable" id='optionsMenu' onClick={displayOptions}>
+                        <img src={menu} alt='Menu'/>
+                    </button>
+                    <div className='navbar-condensed-dropdown'>
+                        <div className="navbar-individual-link">
+                            <button className="navbar-clickable" onClick={buttonClickHandler}>About</button>
+                        </div>
+                        <div className="navbar-individual-link">
+                            <button className="navbar-clickable" onClick={buttonClickHandler}>Projects</button>
+                        </div>
+                        <div className="navbar-individual-link">
+                            <button className="navbar-clickable" onClick={buttonClickHandler}>Contact</button>
+                        </div>
+                        <div className="navbar-individual-link">
+                            <button className="navbar-clickable" id="navbar-resume" onClick={buttonClickHandler}>Resume</button>
+                        </div>
+                    </div>
+                </div>
+            );
+
+            // Add condensed class to navbar
+            navbar.classList.toggle('condensed');
+        }
+    }, [navbarSmall])
+
+    // Monitors change in viewport width
+    const navbarMenuMonitor = () => {
+
+        // If the width is less than 400px 
+        if (window.innerWidth < 400) setNavbarSmall(true);
+
+        // If the width is more than 400px
+        else if (window.innerWidth >= 400) setNavbarSmall(false);
+        
+    }
 
     // Calculates where to scroll
     const scrollIntoViewWithOffset = (selector) => {
@@ -21,6 +124,10 @@ const NavBar = () => {
       }
 
     const buttonClickHandler = (e) => {
+
+        const openOptionsMenu = document.querySelector('.navbar-condensed-dropdown.open');
+        // Remove options menu if it is there
+        if (openOptionsMenu) openOptionsMenu.classList.toggle('open');
 
         // Home
         if (e.target.id === 'homeBtnImg') scrollIntoViewWithOffset('.header-container');
@@ -54,6 +161,13 @@ const NavBar = () => {
         
     }
 
+    // Displays dropdown menu on devices < 400px wide
+    const displayOptions = () => {
+        // Get options menu div
+        const optionsMenu = document.querySelector('.navbar-condensed-dropdown');
+        optionsMenu.classList.toggle('open');
+    }
+
     return (
         <div className="navbar">
             <div className='left-links'>
@@ -70,20 +184,9 @@ const NavBar = () => {
                     </label>
                 </div>
             </div>
-            <div className="navbar-links">
-                <div className="navbar-individual-link">
-                    <button className="navbar-clickable" onClick={buttonClickHandler}>About</button>
-                </div>
-                <div className="navbar-individual-link">
-                    <button className="navbar-clickable" onClick={buttonClickHandler}>Projects</button>
-                </div>
-                <div className="navbar-individual-link">
-                    <button className="navbar-clickable" onClick={buttonClickHandler}>Contact</button>
-                </div>
-                <div className="navbar-individual-link">
-                    <button className="navbar-clickable" id="navbar-resume" onClick={buttonClickHandler}>Resume</button>
-                </div>
-            </div>
+
+            {navbarLinks}
+
         </div>
     )
 
