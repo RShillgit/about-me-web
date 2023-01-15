4f4f7d26de8481d91cbe1aa0b41892be85d6be0c
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-01-15 17:23:43 -0500

    Message sent confirmation and light/dark mode colors

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import '../styles/contact.css';
 import $ from 'jquery';
+import check from '../images/check.png';
 
 /*
     Add an animation after clicking submit button that closes the textarea,
@@ -12,6 +13,7 @@ import $ from 'jquery';
 const Contact = () => {
 
     const [messageText, setMessageText] = useState('');
+    const [messageSentConfirmation ,setMessageSentConfirmation] = useState('');
 
     // Disable/Enable message button
     useEffect(() => {
@@ -42,9 +44,51 @@ const Contact = () => {
             data: {
                 message: messageText
             },
-            success: (data) => console.log(data),
+            // If email is successful, 
+            success: () => {
+                messageSentAnimation();
+            },
             error: (err) => console.log(err)
          });
+    }
+
+    const messageSentAnimation = () => {
+
+        // Toggle 'sent' class on textarea to render animation
+        const textArea = document.querySelector('.messageTextArea');
+        textArea.classList.toggle('sent');
+
+        // After 1 second (the duration of the animation)
+        setTimeout(() => {
+
+            //  Render confirmation message
+            setMessageSentConfirmation(
+                <div className='messageSentConfirmation'>
+                    <h4>Message Sent to Robert.Siciliano22@gmail.com!</h4>
+                    <img src={check} alt=''/>
+                </div>
+            )
+
+            // Remove textarea 
+            textArea.style.display = 'none';
+
+            // Remove 'sent' class
+            textArea.classList.toggle('sent');
+        }, 1000)
+
+        // After 4 seconds
+        setTimeout(() => {
+
+            // Remove convfirmation message
+            setMessageSentConfirmation('')
+
+            // Reset the message text
+            setMessageText('');
+            textArea.value = '';
+
+            // Rerender text area
+            textArea.style.display = 'block';
+        }, 4000)
     }
 
 
@@ -65,7 +109,10 @@ const Contact = () => {
 
                 <input type="hidden" name="_next" value="localhost:3000"></input>
 
-                <textarea onChange={handleTextAreaChange} placeholder="Leave me a meessage" name='Message'/>
+                <textarea className='messageTextArea' onChange={handleTextAreaChange} placeholder="Leave me a meessage" name='Message'/>
+                
+                {messageSentConfirmation}
+
                 <div className='sendMessageDiv'>
                     <button type='submit' id='sendMessageBtn' onClick={sendMessage}>Message</button>
                 </div>
