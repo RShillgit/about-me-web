406b8acb161546d5784eccd463f2426a1d5dd45b
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-01-19 16:42:37 -0500

    Light mode blue change

--- src/components/footer.js
+++ src/components/footer.js
@@ -30,7 +30,7 @@ const Footer = () => {
 
             <div className="footer-content">
                 <a href='https://www.linkedin.com/in/robert-siciliano-120379240/' target="_blank" rel="noopener noreferrer">
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt='Linkedin'/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt='Linkedin' id='linkedin'/>
                 </a>
                 <a href='https://github.com/RShillgit' target="_blank" rel="noopener noreferrer">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='Github' id='github'/>
