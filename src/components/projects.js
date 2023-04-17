0c8ab1476eea46bc80a72d017151a9cf79071a76
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-04-17 10:51:53 -0400

    Fakebook project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,5 @@
 import '../styles/projects.css';
+import fakebookDemo from '../videos/Fakebook Demo.mp4';
 import blogApiDemo from '../videos/Blog API Demo.mp4';
 import membersOnlyDemo from '../videos/Members Only Demo.mp4';
 import phototagDemo from '../videos/Photo Tag Demo.mp4';
@@ -6,6 +7,39 @@ import phototagDemo from '../videos/Photo Tag Demo.mp4';
 const Projects = () => {
     return (
         <div className="projects-container">
+
+            <div className='project-example'>
+                <div className='project-title'>
+                    <h3>Fakebook</h3>
+                </div>
+                <div className='project-demo'>
+                    <video autoPlay loop muted id='phototagDemo'>
+                        <source src={fakebookDemo} type="video/mp4" />
+                        <source src="movie.ogg" type="video/ogg" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+
+                <div className='project-description'>
+                    <button className='project-tech-stack' disabled={true}>Node.js</button>
+                    <button className='project-tech-stack' disabled={true}>React</button>
+                    <button className='project-tech-stack' disabled={true}>CSS</button>
+                    <button className='project-tech-stack' disabled={true}>MongoDB</button>
+                </div>
+
+                <div className='project-comments'>
+                    <p>The demo may take a few moments to spin up</p>
+                </div>
+
+                <div className='project-buttons'>
+                    <a href='https://github.com/RShillgit/Fakebook' target="_blank" rel="noopener noreferrer">
+                        <button>Repo</button>
+                    </a>
+                    <a href='https://deadpan-meal-production.up.railway.app' target="_blank" rel="noopener noreferrer">
+                        <button>Demo</button>
+                    </a>
+                </div>
+            </div>
 
             <div className='project-example'>
                 <div className='project-title'>
@@ -22,8 +56,8 @@ const Projects = () => {
                 <div className='project-description'>
                     <button className='project-tech-stack' disabled={true}>Node.js</button>
                     <button className='project-tech-stack' disabled={true}>React</button>
-                    <button className='project-tech-stack' disabled={true}>MongoDB</button>
                     <button className='project-tech-stack' disabled={true}>CSS</button>
+                    <button className='project-tech-stack' disabled={true}>MongoDB</button>
                 </div>
 
                 <div className='project-comments'>
@@ -55,8 +89,8 @@ const Projects = () => {
 
                 <div className='project-description'>
                     <button className='project-tech-stack' disabled={true}>Node.js</button>
-                    <button className='project-tech-stack' disabled={true}>MongoDB</button>
                     <button className='project-tech-stack' disabled={true}>CSS</button>
+                    <button className='project-tech-stack' disabled={true}>MongoDB</button>
                 </div>
 
                 <div className='project-comments'>
@@ -68,33 +102,6 @@ const Projects = () => {
                         <button>Repo</button>
                     </a>
                     <a href='https://members-only-hfy4.onrender.com' target="_blank" rel="noopener noreferrer">
-                        <button>Demo</button>
-                    </a>
-                </div>
-            </div>
-
-            <div className='project-example'>
-                <div className='project-title'>
-                    <h3>Photo Tag</h3>
-                </div>
-                <div className='project-demo'>
-                    <video autoPlay loop muted id='phototagDemo'>
-                        <source src={phototagDemo} type="video/mp4" />
-                        <source src="movie.ogg" type="video/ogg" />
-                        Your browser does not support the video tag.
-                    </video>
-                </div>
-                <div className='project-description'>
-                    <button className='project-tech-stack' disabled={true}>React</button>
-                    <button className='project-tech-stack' disabled={true}>CSS</button>
-                    <button className='project-tech-stack' disabled={true}>Firebase</button>
-                </div>
-
-                <div className='project-buttons'>
-                    <a href='https://github.com/RShillgit/photo-tag' target="_blank" rel="noopener noreferrer">
-                        <button>Repo</button>
-                    </a>
-                    <a href='https://rshillgit.github.io/photo-tag/' target="_blank" rel="noopener noreferrer">
                         <button>Demo</button>
                     </a>
                 </div>
