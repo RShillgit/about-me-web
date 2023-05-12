35fe08a8e206ee2bfeacf7119b0e8f2a0c54a5e2
Robert Siciliano <Robert.siciliano22@gmail.com>
2023-05-12 17:52:37 -0400

    Replaced Members-Only with Food Logger

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,7 @@
 import '../styles/projects.css';
 import fakebookDemo from '../videos/Fakebook Demo.mp4';
 import blogApiDemo from '../videos/Blog API Demo.mp4';
-import membersOnlyDemo from '../videos/Members Only Demo.mp4';
+import foodLoggerDemo from '../videos/Food Logger Demo.mp4';
 
 const Projects = () => {
     return (
@@ -41,6 +41,40 @@ const Projects = () => {
             </div>
 
             <div className='project-example'>
+
+            <div className='project-title'>
+                <h3>Food Logger</h3>
+            </div>
+                <div className='project-demo'>
+                    <video autoPlay loop muted id='foodLoggerDemo'>
+                        <source src={foodLoggerDemo} type="video/mp4" />
+                        <source src="movie.ogg" type="video/ogg" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+
+                <div className='project-description'>
+                    <button className='project-tech-stack' disabled={true}>Node.js</button>
+                    <button className='project-tech-stack' disabled={true}>React</button>
+                    <button className='project-tech-stack' disabled={true}>CSS</button>
+                    <button className='project-tech-stack' disabled={true}>MongoDB</button>
+                </div>
+
+                <div className='project-comments'>
+                    <p>The demo may take a few moments to spin up</p>
+                </div>
+
+                <div className='project-buttons'>
+                    <a href='https://github.com/RShillgit/Food-Logger' target="_blank" rel="noopener noreferrer">
+                        <button>Repo</button>
+                    </a>
+                    <a href='https://food-logger-production.up.railway.app' target="_blank" rel="noopener noreferrer">
+                        <button>Demo</button>
+                    </a>
+                </div>
+            </div>
+
+            <div className='project-example'>
                 <div className='project-title'>
                     <h3>Blog API</h3>
                 </div>
@@ -68,39 +102,6 @@ const Projects = () => {
                         <button>Repo</button>
                     </a>
                     <a href='https://rshill-blog-production.up.railway.app/' target="_blank" rel="noopener noreferrer">
-                        <button>Demo</button>
-                    </a>
-                </div>
-            </div>
-
-            <div className='project-example'>
-
-                <div className='project-title'>
-                    <h3>Members Only</h3>
-                </div>
-                <div className='project-demo'>
-                    <video autoPlay loop muted id='membersOnlyDemo'>
-                        <source src={membersOnlyDemo} type="video/mp4" />
-                        <source src="movie.ogg" type="video/ogg" />
-                        Your browser does not support the video tag.
-                    </video>
-                </div>
-
-                <div className='project-description'>
-                    <button className='project-tech-stack' disabled={true}>Node.js</button>
-                    <button className='project-tech-stack' disabled={true}>CSS</button>
-                    <button className='project-tech-stack' disabled={true}>MongoDB</button>
-                </div>
-
-                <div className='project-comments'>
-                    <p>The demo may take a few moments to spin up</p>
-                </div>
-
-                <div className='project-buttons'>
-                    <a href='https://github.com/RShillgit/Members-Only' target="_blank" rel="noopener noreferrer">
-                        <button>Repo</button>
-                    </a>
-                    <a href='https://members-only-hfy4.onrender.com' target="_blank" rel="noopener noreferrer">
                         <button>Demo</button>
                     </a>
                 </div>
